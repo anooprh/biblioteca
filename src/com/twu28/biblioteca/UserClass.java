0b2266426bf0a07d182c8e09f5f031d5922bafe0
anoophallur <anoophallur@gmail.com>
2012-07-18 12:14:51 +0530

    Few More tests have been added

--- src/com/twu28/biblioteca/UserClass.java
+++ src/com/twu28/biblioteca/UserClass.java
@@ -4,8 +4,8 @@ package com.twu28.biblioteca;
  * Created with IntelliJ IDEA.
  * User: Administrator
  * Date: 7/17/12
- * Time: 7:58 PM
+ * Time: 10:26 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Library {
+public class UserClass {
 }
